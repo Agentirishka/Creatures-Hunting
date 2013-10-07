5bfc0649459613e1fcc2a5f41b5908ac74c85021
Irina <irina@irishka>
2013-10-07 15:13:25 +0200

    dead

--- CreatureHunting/src/main/java/com/muni/fi/pa165/creaturehunting/Weapon.java
+++ CreatureHunting/src/main/java/com/muni/fi/pa165/creaturehunting/Weapon.java
@@ -21,6 +21,7 @@ public class Weapon implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
+    private String description;
 
     public Long getId() {
         return id;
